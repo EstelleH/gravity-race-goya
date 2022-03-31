bedf2d678224d43dd36447137b42776d47d485b4
Estelle <estelle@felix-creation.fr>
2022-04-01 00:49:19 +0200

    Mise en ligne du thème.

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Goya
+ */
+
+ get_header(); ?>
+
+<div class="type-page page-padding">
+<div class="container">
+	<div class="row content404">
+		<div class="col-12">
+			<div class="empty-circle"><?php get_template_part('assets/img/svg/x.svg'); ?></div>
+			<h4><?php esc_html_e( "Désolé mais cette page n'existe pas.", 'goya' ); ?></h4>
+			<a class="button outlined" href="<?php echo esc_url(home_url('/')); ?>"><?php esc_html_e("Revenir à la page d'accueil", 'goya'); ?></a>
+		</div>
+	</div>
+</div>
+</div>
+
+<?php get_footer(); ?>
---
+++ functions.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Child theme functions and definitions.
+ */
+function goya_child_enqueue_styles() {
+wp_enqueue_style( 'goya-style' , get_template_directory_uri() . '/style.css' );    
+    wp_enqueue_style( 'goya-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'goya-style' ),
+        1.0
+    );
+}
+add_action(  'wp_enqueue_scripts', 'goya_child_enqueue_styles', 99 );
+
+function goya_child_javascript() {
+    ?>
+    <!-- Matomo -->
+    <script>
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//matomo.gravity-race.fr/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+    <!-- End Matomo Code -->
+
+    <!-- Start cookieyes banner -->
+    <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/4506d62c78997106e503fcc3/script.js"></script>
+    <!-- End cookieyes banner -->
+    <?php
+}
+add_action('wp_head', 'goya_child_javascript');
+
